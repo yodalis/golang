0c62014fa5ef92d4b7220412e6ef5ffacbca8f57
yodalis <thaiscalazans02@gmail.com>
2024-02-19 21:25:34 -0300

    feat: add first insert in database

---
+++ 6-banco-de-dados/1/go.mod
@@ -0,0 +1,8 @@
+module github.com/yodalis/golang/6-banco-de-dados/1
+
+go 1.21.6
+
+require (
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/google/uuid v1.6.0
+)
---
+++ 6-banco-de-dados/1/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+	// isso é necessário pq a linguagem n escolhe qual driver você vai user
+	// driver = oracle, mysql, postgrees
+	// _ vai deixar a gente não utilizar diretamente o pacote, mesmo que seja necessário, sem isso o go n deixa a gente compilar
+	// o programa sem usar o pacote diretamente
+
+	"github.com/google/uuid"
+)
+
+type Product struct {
+	ID    string
+	Name  string
+	Price float64
+}
+
+func NewProduct(name string, price float64) *Product {
+	return &Product{
+		ID:    uuid.New().String(),
+		Name:  name,
+		Price: price,
+	}
+}
+
+func main() {
+	// formação do segundo parâmetro: user:password@tipoderede(url)/banco
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/goexpert")
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	product := NewProduct("Notebook", 2000)
+
+	err = insertProduct(db, product)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func insertProduct(db *sql.DB, product *Product) error {
+	// Evitando sql injection
+	stmt, err := db.Prepare("insert into products(id, name, price) values(?,?,?)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(product.ID, product.Name, product.Price)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
