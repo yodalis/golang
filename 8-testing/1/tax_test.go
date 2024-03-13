99c97db0a506a6da6575347d15c1ae9246c967ae
Thaís Calazans <64108336+yodalis@users.noreply.github.com>
2024-03-13 23:55:41 +0000

    feat: add fuzz test

--- 8-testing/1/tax_test.go
+++ 8-testing/1/tax_test.go
@@ -69,12 +69,17 @@ func BenchmarkCalculateTax2(b *testing.B) {
 // testing.F de fuzz
 // go test -fuzz=.
 func FuzzCalculateTax(f *testing.F) {
-	seed := []float64{-1, -2, -2.5, 500.0, 1000.0, 1501.0} // aqui estamos dando exemplos do que queremos criar p ele
+	seed := []float64{-1, -2, -2.5, 500.0, 1000.0, 1501.0}
 	for _, amount := range seed {
-		f.Add(amount)
+	 f.Add(amount)
 	}
 	f.Fuzz(func(t *testing.T, amount float64) {
-		result := CalculateTax(amount)
-
+	 result := CalculateTax(amount)
+	 if amount <= 0 && result != 0 {
+		 t.Errorf("Reveived %f but expected 0", result)
+	 }
+	 if amount > 20000 && result != 20 {
+		 t.Errorf("Reveived %f but expected 20", result)
+	 }
 	})
-}
+ }
